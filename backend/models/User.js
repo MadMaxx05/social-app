038880ef38fd23e746117111298d9f1784b635ee
Arseniy Bodnar <arscat28@gmail.com>
2022-06-11 15:17:42 +0300

    Search added

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,18 +1,19 @@
-const {Schema, model} = require('mongoose')
+const { Schema, model } = require('mongoose');
 
+const User = new Schema(
+  {
+    username: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    fullName: { type: String, required: true },
+    country: { type: String, required: true },
+    city: { type: String, required: true },
+    description: { type: String, required: true },
+    interests: { type: Array, required: true },
+    contact: { type: String, required: true },
+  },
+  {
+    versionKey: false,
+  }
+);
 
-const User = new Schema({
-    username: {type: String, unique: true, required: true},
-    password: {type: String, required: true},
-    fullName: {type: String, required: true},
-    country: {type: String, required: true},
-    city: {type: String, required: true},
-    description: {type: String, required: true},
-    interests: {type: Array, required: true},
-    contact: {type: String, required: true}},
-    {
-        versionKey: false
-    }
-)
-
-module.exports = model('User', User)
+module.exports = model('User', User);
